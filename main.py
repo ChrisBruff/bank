a57f020bf50252a92bc895cf253092ea90639db7
chris bruff <84159535+ChrisBruff@users.noreply.github.com>
2023-04-29 12:06:32 -0700

    - Added password attributes. - To Be added:     - make user and password verification a part of main bank class.     - have main loop reference the instance and get the instantiation.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ child: Bank
 
  - account balance
  - amount of money
- - can deposit, withdraw, view_balance test123
+ - can deposit, withdraw, view_balance
 '''
 
 
@@ -25,11 +25,13 @@ class User():
 
 
 class TheBank(User):
-    def __init__(self,name,age,gender):
+    def __init__(self,name,age,gender,password):
         super().__init__(name,age,gender)
         self.balance = 0
+        self.password = password
 
     def deposit(self,amount):
+        print(self.password)
         self.amount = amount
         self.balance = self.balance + self.amount
         print("Your New Balance is: $",self.balance)
@@ -45,13 +47,19 @@ class TheBank(User):
 
 x = 1
 
-Chris = TheBank('Chris', 27, 'Male')
+Chris = TheBank('Chris', 27, 'Male','password')
 
-while (x == 1):
+# print((Chris.password)) used to test feature
+'''
+Features to be added: Username and pass word auth
+    - make user and password verification a part of main bank class.
+    - have main loop reference the instance and get the instantiation. 
+'''
+def bankAction():
     choice = 0
     choice = int(input("1 for deposit, 2 for withdraw"))
 
-    if(choice == 1):
+    if (choice == 1):
 
         amt = int(input("What is your deposit?"))
         Chris.deposit(amt)
@@ -62,3 +70,13 @@ while (x == 1):
         Chris.withdraw(amt)
 
     x = int(input("Exit? Enter any number but 1"))
+
+while (x == 1):
+    password = input("please input your password:")
+
+    if password == Chris.password:
+        bankAction()
+
+    else:
+        print("incorrect password")
+        password = input("please input your password:")
